8ec1d18e165bef5849ac966f9567d985cd0c8b9d
alexlipa91 <alessandroliparoti@live.it>
2015-06-15 19:48:51 +0200

    demo with social buttons
    added social buttons as png in the logo fragment

--- gen/com/spotime/demo/R.java
+++ gen/com/spotime/demo/R.java
@@ -116,13 +116,10 @@ containing a value of this type.
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f080006;
-        public static final int action_settings=0x7f08000b;
+        public static final int action_settings=0x7f080008;
         public static final int anchored=0x7f080000;
-        public static final int button1=0x7f080007;
-        public static final int button2=0x7f080008;
-        public static final int button3=0x7f080009;
         public static final int collapsed=0x7f080001;
-        public static final int container=0x7f08000a;
+        public static final int container=0x7f080007;
         public static final int expanded=0x7f080002;
         public static final int hidden=0x7f080003;
         public static final int pager=0x7f080004;
